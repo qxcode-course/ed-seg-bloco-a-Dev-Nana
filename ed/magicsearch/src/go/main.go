1e1bbfc4e9571b44b336bb47d8bf877d8226f4f7
Geovana Veras <evylaveras@gmail.com>
2026-06-15 17:51:30 +0000

    up magicsearch

---
+++ ed/magicsearch/src/go/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func MagicSearch(slice []int, value int) int {
+	low := 0
+	high := len(slice)
+
+	for low < high {
+		mid := (low + high) / 2
+		if slice[mid] < value {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+	
+	if low < len(slice) && slice[low] == value {
+		pos := low
+		for pos+1 < len(slice) && slice[pos+1] == value {
+			pos++
+		}
+		return pos
+	}
+
+	return low
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	parts := strings.Fields(scanner.Text())
+	slice := make([]int, 0, 1)
+	for _, elem := range parts[1 : len(parts)-1] {
+		value, _ := strconv.Atoi(elem)
+		slice = append(slice, value)
+	}
+
+	scanner.Scan()
+	value, _ := strconv.Atoi(scanner.Text())
+	result := MagicSearch(slice, value)
+	fmt.Println(result)
+}
