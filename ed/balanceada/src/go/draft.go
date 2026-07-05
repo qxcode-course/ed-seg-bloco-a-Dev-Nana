200e9fc074ea28f13f7cccfc90cbfa397e1f5a55
Dev-Nana <evylaveras@gmail.com>
2026-07-04 22:56:01 -0300

    balanceada

--- ed/balanceada/src/go/draft.go
+++ ed/balanceada/src/go/draft.go
@@ -1,6 +1,45 @@
 package main
-import "fmt"
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
 func main() {
-    
-    
+    scanner := bufio.NewScanner(os.Stdin)
+
+    scanner.Scan()
+    expr := scanner.Text()
+
+    pilha := []rune{}
+
+    for _, c := range expr {
+
+        if c == '(' || c == '[' {
+            pilha = append(pilha, c)
+        } else {
+            if len(pilha) == 0 {
+                fmt.Println("nao balanceado")
+                return
+            }
+            
+            topo := pilha[len(pilha)-1]
+            pilha = pilha[:len(pilha)-1]
+
+            if (c == ')' && topo != '(') || (c == ']' && topo != '[') {
+                fmt.Println("nao balanceado")
+                return
+            }
+        }
+    }
+
+    if len(pilha) == 0 {
+
+        fmt.Println("balanceado")
+        
+    } else {
+        
+        fmt.Println("nao balanceado")
+
+    }
 }
