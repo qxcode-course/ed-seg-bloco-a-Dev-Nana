008a07cc77a7cd86046bcd329d510355d5a5b926
Dev-Nana <evylaveras@gmail.com>
2026-07-05 18:30:22 -0300

    tentando atividades de arvore

--- ed/distancia/src/go/draft.go
+++ ed/distancia/src/go/draft.go
@@ -1,5 +1,57 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "bufio"
+    "os"
+)
+
+func valido(seq []rune, pos int, valor rune, limite int) bool{
+    for i := pos - limite; i <= pos+limite; i++ {
+        if i < 0 || i >= len(seq) || i == pos {
+            continue
+        }
+        if seq[i] == valor {
+            return false
+        }
+    }
+    return true
+}
+
+func resolver(seq []rune, limite, index int) bool{
+    if index == len(seq){
+        return true
+    }
+
+    if seq[index] != '.' {
+        return resolver(seq, limite, index+1)
+    }
+
+    for num := 0; num <= limite; num++ {
+        valor := rune('0' + num)
+
+        if valido(seq, index, valor, limite){
+            seq[index] = valor
+
+            if resolver(seq, limite, index+1) {
+                return true
+            }
+            seq[index] = '.'
+        }
+    }
+
+    return false
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    seq := []rune(scanner.Text())
+
+    var limite int
+    scanner.Scan()
+    fmt.Sscan(scanner.Text(), &limite)
+
+    resolver(seq, limite, 0)
+
+    fmt.Println(string(seq))
 }
--- ed/wordsearch/src/go/main.go
+++ ed/wordsearch/src/go/main.go
@@ -8,6 +8,46 @@ import (
 
 // Não mude a assinatura desta função, ela é a função chamada pelo LeetCode
 func exist(grid [][]byte, word string) bool {
+	nl := len(grid)
+	nc := len(grid[0])
+
+	var dfs func(l, c, index int) bool
+
+	dfs = func(l, c, index int) bool {
+
+		if index == len(word) {
+			return true
+		}
+
+		if l < 0 || l >= nl || c < 0 || c >= nc {
+			return false
+		}
+
+		if grid[l][c] != word[index] {
+			return false
+		}
+
+		temp := grid[l][c]
+		grid[l][c] = '#'
+
+		if dfs(l-1, c, index+1) || dfs(l+1, c, index+1) || dfs(l, c-1, index+1) || dfs(l, c+1, index+1) {
+			grid[l][c] = temp
+			return true
+		}
+
+		grid[l][c] = temp
+
+		return false
+	}
+
+	for i := 0; i < nl; i++{
+		for j := 0; j < nc; j++{
+			if dfs(i, j, 0) {
+				return true
+			}
+		}
+	}
+
 	_, _ = grid, word
 	return false
 }
@@ -27,3 +67,4 @@ func main() {
 		fmt.Println("false")
 	}
 }
+
